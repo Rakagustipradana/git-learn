924650cf68b61aa5fea95f03cbe67b2de4271fa0
evansrantelino@gmail.com <evansrantelino@gmail.com>
2025-02-20 14:20:28 +0700

    first commit

---
+++ cobaGitPus.ts
@@ -0,0 +1,14 @@
+const carName: string[] = ["Sedan", "Truck", "Mini Bus"]
+let f = 0;
+let  sdsd= "";
+ 
+ while (f <= 5){
+    // text += "The number is " + i;
+    console.log("The number is " + f)
+    f++;
+  }
+
+function dsdsdsd(): string{
+    let dsds:string = "4";
+    return dsds; 
+}
